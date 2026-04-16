dfd5cf0bab5009e4064258ef93f316273a774cef
kylerhanson <kyhans07@wsc.edu>
2026-04-16 14:37:47 -0500

    fixed typo + added jsdocs

--- movie_tracker/modules/movie.js
+++ movie_tracker/modules/movie.js
@@ -1,12 +1,22 @@
 class Movie {
+    /**
+     * Creates a new Movie instance.
+     * @param {string} title - The title of the movie.
+     * @param {string} genre - The category/genre of the movie.
+     * @param {number|string} rating - The user rating (1-10).
+     */
     constructor(title, genre, rating) {
         this.title = title;
         this.genre = genre;
         this.rating = rating;
     }
 
+    /**
+     * Returns a string representation of the movie for display in the list.
+     * @returns {string} Formatted movie details.
+     */
     toString() {
-        return `${this.title} | ${this.genre | rating=${this.rating}`;
+        return `${this.title} | ${this.genre} | Rating: ${this.rating}`;
     }
 }
 
