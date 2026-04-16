bf9dc23d1a67b522e97c0b8de3a195ad915b9c25
kylerhanson <kyhans07@wsc.edu>
2026-04-16 15:00:58 -0500

    added description and JSdocs

--- movie_tracker/modules/movie_storage.js
+++ movie_tracker/modules/movie_storage.js
@@ -1,7 +1,17 @@
+/**
+ * Description: Data access object for movie storage using local storage.
+ * Author: Kyler Hanson
+ * Date: 2026-04-16
+ * GitHub: https://github.com/kyhans07
+ */
 import * as storage from 'storage';
 import Movie from 'task';
 
 const movieStorage = {
+    /**
+     * Retrieves movies from local storage and converts them to Movie instances.
+     * @returns {Array} An array of Movie objects.
+     */
     retrieve() {
         const movies = [];
         const movieArray = storage.retrieve("Movies");
@@ -13,6 +23,10 @@ const movieStorage = {
         }
         return movies;
     },
+    /**
+     * Saves the current movie collection to local storage.
+     * @param {Array} movies - The array of movies to store.
+     */
     store(movies) {
         storage.store("Movies", Array.from(movies));
     },
